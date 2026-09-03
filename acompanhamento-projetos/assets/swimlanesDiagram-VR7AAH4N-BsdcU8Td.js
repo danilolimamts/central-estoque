dbef0da2c091bd9ba407b5adcd86eee443c95e6b
danilolimamts <danilomatoslima123@gmail.com>
2026-09-02 22:02:34 -0300

    Projetos: marca em destaque no cabeçalho (#119)
    Logo de 48 px para 72 px no desktop (56 px no celular), traço vertical separando marca e título, e o título do módulo de 18 px para 24 px.

---
+++ acompanhamento-projetos/assets/swimlanesDiagram-VR7AAH4N-BsdcU8Td.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-NK18NmGA.js";import{_ as a}from"./mermaid.core-C6NfbLtt.js";import"./chunk-5VM5RSS4-CeuNZ0p9.js";import"./chunk-XXDRQBXY-C-pyThlG.js";import"./chunk-POPQ4Y6H-CBCuabef.js";import"./chunk-F27PBJKO-uH_rpSMY.js";import"./channel-leAydAPB.js";import"./index-B_wLX0Xj.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
